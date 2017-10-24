e55e3faaa1e66e14c04f4b4c2e68338eb6e8cf43
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-24 10:43:14 -0700

    add ingredients to a recipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,10 +22,10 @@ export class AppComponent {
 }
 
 export class Recipe {
-  constructor(public name: string, public steps: string[]) {}
+  constructor(public name: string, public ingredients: string[], public steps: string[]) {}
 }
 
 const RECIPES: Recipe[] = [
-  new Recipe("Cookies", ["In a medium sized bowel mix flour, baking soda, and salt. Set aside.", "In a large bowl mix butter, egg, vanilla, and sugars.", "Slowly combine the flour mixture into the large bowl.", "Add chocolate chips and bake at 350 for 9-12 minutes or until golden brown."]),
-  new Recipe("Yellow Thai Curry", ["Saute onions in olive oil until translucent.", "Toss in chicken and cook until white.", "Add yellow curry paste.", "Add coconut milk.", "Bring mixture up to heat and toss in potatos and carrots.", "For additional flavor add a few drops of fish sauce.", "Cook for 25 minutes or until potatoes are tender."])
+  new Recipe("Cookies", ["Flour", "Sugar", "Brown Sugar", "Eggs", "Butter", "Salt", "Baking Soda", "Vanilla", "Chocolate Chips"], ["In a medium sized bowel mix flour, baking soda, and salt. Set aside.", "In a large bowl mix butter, egg, vanilla, and sugars.", "Slowly combine the flour mixture into the large bowl.", "Add chocolate chips and bake at 350 for 9-12 minutes or until golden brown."]),
+  new Recipe("Yellow Thai Curry", ["Chicken", "Curry Paste", "Coconut Milk", "Curry Paste (Mae Ploy)", "Onion", "Carrots/Parsnips", "Potatos", "Fish Sauce (optional)"], ["Saute onions in olive oil until translucent.", "Toss in chicken and cook until white.", "Add yellow curry paste.", "Add coconut milk.", "Bring mixture up to heat and toss in potatos and carrots.", "For additional flavor add a few drops of fish sauce.", "Cook for 25 minutes or until potatoes are tender."])
 ];
